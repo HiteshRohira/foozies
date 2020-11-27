0c15c584ba299e02d91b551ce4fbda364bc58e1e
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-11-27 20:29:48 +0530

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav>
+			<h2>foozies</h2>
+			<ul>
+				<li>
+					<i className="material-icons">search</i>
+				</li>
+				<li>
+					<i className="material-icons">shopping_bag</i>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
